8e1b4be5d92b15765272423470a1f36730823506
blockchain1124 <blockchain1124@gmail.com>
2018-12-10 10:37:52 +0900

    commit 10

--- src/blockchain.js
+++ src/blockchain.js
@@ -25,9 +25,11 @@ let blockchain = [genesisBlock];
 const getLastBlock = () => blockchain[blockchain.length - 1];
 // 블록생성시점 타임스템프 가져오기
 const getTimestamp = () => new Date().getTimeStamp / 1000;
+// 블록체인 가져오기
+const getBlockChain = () => blockchain;
 // 블록생성시 필요한 hash 생성
 const createHash = (index, previousHash, timestamp, data) =>
-    cryptoJS(index+previousHash+timestamp+data).toString();
+    cryptoJS(index+previousHash+timestamp+JSON.stringify(data).toString());
 //신규블록 생성
 const createNewBlock = data => {
     const previousBlock = getLastBlock();
@@ -49,4 +51,73 @@ const createNewBlock = data => {
     )
     return newBlock;
 };
-console.log(blockchain);
+
+// 블록 해쉬 가져오기
+const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data);
+
+// 신규생성된 블록 유효성 검증
+const isNewBlockValid = (candidateBlock, lastestBlock) => {
+    if(!isNewStructureValid(candidateBlock)){
+        console.log("후보블록의 구조가 유효하지 않습니다.");
+        return false;
+    }else if(lastestBlock.index + 1 !== candidateBlock.index){
+        console.log("후보블록의 인덱스가 유효하지 않습니다.");
+        return false;
+    }else if(lastestBlock.hash !== candidateBlock.previousHash){
+        console.log("후보블록의 이전블록해시가 최근블록 해쉬와 맞지 않습니다.");
+        return false;
+    }else if(getBlockHash(candidateBlock) !== candidateBlock.hash){
+        console.log("후보블록의 해쉬가 유효하지 않습니다.")
+        return false;
+    }
+    return true;
+}
+
+// 블록 구조 유효성 검증
+const isNewStructureValid = (block) => {
+    retun(
+        typeof block.index === "number" &&
+        typeof block.hash === "string" &&
+        typeof block.previousHash === "string" &&
+        typeof block.timestamp === "number" &&
+        typeof block.data === "string"
+    )
+}
+
+// 체인의 유효성 검증
+const isChainValid = (candidateChain) => {
+    const isGenesisValid = block => {
+        return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    }
+    if(!isGenesisValid(candidateChain[0])){
+        console.log("후보체인의 제네시스블록이 유효하지 않습니다.");
+        return false;
+    }
+    // 제네시스 블록은 제외하고 검증시작
+    for (let i = 1; i < candidateChain.length; i++) {
+        if(isNewBlockValid(candidateChain[i], candidateChain[i-1])){
+            return false;
+        } 
+    }
+    return true;
+}
+
+// 항상 가장긴 체인을 선택
+const replaceChain = candidateChain => {
+    if(isChainValid(candidateChain) && candidateChain.length > getBlockChain().length){
+        blockchain = candidateChain;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// 체인에 블록을 추가
+const addBlockToChain = candidateBlock => {
+    if(isNewBlockValid(candidateBlock, getLastBlock())){
+        getBlockChain().push(candidateBlock);
+        return true;
+    }else{
+        return false;
+    }
+}
